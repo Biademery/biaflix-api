a3f78fecd09df271bd3353b890acb0a55988b1f7
Biademery <biademery@gmail.com>
2023-02-23 08:51:00 -0300

    cors

--- api/settings.py
+++ api/settings.py
@@ -10,8 +10,9 @@ SECRET_KEY = 'django-insecure-ti#0^6+kcfb1=yjkw67ot7z=pwzscbcxxj7a8utl(_71elh16d
 DEBUG = True
 
 ALLOWED_HOSTS=['*']
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = ['https://biaflix.vercel.app/']
+CSRF_TRUSTED_ORIGINS = ['https://biaflix.vercel.app/',]
 
 
 # Application definition
